2266373b5414e0b2fb366487e45c2eb247b23872
komoto <komoto@jeol.co.jp>
2021-04-28 17:15:43 +0900

    Add setup file and one demo code (debut)

---
+++ debut.py
@@ -0,0 +1,86 @@
+#! python
+# -*- coding: utf-8 -*-
+"""deb utility
+
+Author: Kazuya O'moto <komoto@jeol.co.jp>
+"""
+from __future__ import (division, print_function,
+                        absolute_import, unicode_literals)
+import mwx
+import numpy as np
+
+np.set_printoptions(linewidth=256) # default 75
+
+
+def init_spec(self):
+    """Initialize shell/editor and the environs
+    """
+    @self.define_key('C-tab')
+    def insert_space_like_tab():
+        """タブの気持ちになって半角スペースを前向きに入力する
+        Enter half-width spaces forward as if feeling like a tab
+        """
+        self.eat_white_forward()
+        
+        _text, lp = self.CurLine
+        n = lp % 4
+        self.write(' ' * (4-n))
+    
+    @self.define_key('C-S-tab')
+    def delete_backward_space_like_tab():
+        """シフト+タブの気持ちになって半角スペースを後ろ向きに消す
+        Delete half-width spaces backward as if feeling like a shift+tab
+        """
+        self.eat_white_forward()
+        
+        _text, lp = self.CurLine
+        n = lp % 4 or 4
+        for i in range(n):
+            p = self.cur
+            if self.preceding_char == ' ' and p != self.bol:
+                self.Replace(p-1, p, '')
+            else:
+                break
+    
+    self.set_style({
+        "STC_STYLE_DEFAULT"     : "fore:#cccccc,back:#202020,face:MS Gothic,size:9",
+        "STC_STYLE_CARETLINE"   : "fore:#ffffff,back:#012456,size:2",
+        "STC_STYLE_LINENUMBER"  : "fore:#000000,back:#f0f0f0,size:9",
+        "STC_STYLE_BRACELIGHT"  : "fore:#ffffff,back:#202020,bold",
+        "STC_STYLE_BRACEBAD"    : "fore:#ffffff,back:#ff0000,bold",
+        "STC_STYLE_CONTROLCHAR" : "size:9",
+        "STC_P_DEFAULT"         : "fore:#cccccc,back:#202020",
+        "STC_P_IDENTIFIER"      : "fore:#cccccc",
+        "STC_P_COMMENTLINE"     : "fore:#42c18c,back:#004040",
+        "STC_P_COMMENTBLOCK"    : "fore:#42c18c,back:#004040,eol",
+        "STC_P_CHARACTER"       : "fore:#a0a0a0",
+        "STC_P_STRING"          : "fore:#a0a0a0",
+        "STC_P_TRIPLE"          : "fore:#a0a0a0,back:#004040,eol",
+        "STC_P_TRIPLEDOUBLE"    : "fore:#a0a0a0,back:#004040,eol",
+        "STC_P_STRINGEOL"       : "fore:#808080",
+        "STC_P_WORD"            : "fore:#80a0ff",
+        "STC_P_WORD2"           : "fore:#ff80ff",
+        "STC_P_DEFNAME"         : "fore:#e0c080,bold",
+        "STC_P_CLASSNAME"       : "fore:#e0c080,bold",
+        "STC_P_DECORATOR"       : "fore:#e08040",
+        "STC_P_OPERATOR"        : "",
+        "STC_P_NUMBER"          : "fore:#ffc080",
+    })
+    self.wrap(0)
+
+
+def dive(*args):
+    """Dive into the process, from your diving point.
+To Divers:
+    This executes your startup script ($PYTHONSTARTUP:~/.py).
+    Then, call spec (post-startup function defined above),
+    """
+    mwx.deb(*args, startup=init_spec, execStartupScript=True,
+        introText = """
+        Anything one man can imagine, other man can make real.
+        --- Jules Verne (1828--1905)
+        """)
+
+
+if __name__ == '__main__':
+    dive()
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #! python
 # -*- coding: utf-8 -*-
 from setuptools import setup 
-from Lib.mwx import __version__
+from Lib.mwx import __version__, __author__
 
 setup(
     name = "mwxlib",
     version = __version__,
-    author = "Kazuya O'moto",
+    author = __author__,
     author_email = "komoto@jeol.co.jp",
-    description = "An wrapper of wxPython (phoenix)",
+    description = "An wrapper of matplotlib and wxPython (phoenix)",
     
     ## Description of the package in the distribution
     package_dir = {
@@ -21,11 +21,10 @@ setup(
     ],
     
     ## install_requires = [
-    ##     'wxPython',
-    ##     'scipy',
-    ##     'Pillow',
+    ##     'numpy',
+    ##     'pillow',
     ##     'matplotlib',
-    ##     'opencv-python',
+    ##     'wxpython==4.0.7',
     ## ],
     
     include_package_data = True,
