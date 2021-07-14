75df7dd19935c7ed45a46c0ce1e2c93b9387e381
komoto <komoto@jeol.co.jp>
2021-07-14 16:06:39 +0900

    Update setup env

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@
 from setuptools import setup 
 from Lib.mwx import __version__, __author__
 
-try:
-    with open('README.rst', encoding='utf-8') as f:
-        readme = f.read()
-except IOError:
-    readme = ''
-
 setup(
     name = "mwxlib",
     version = __version__,
@@ -16,8 +10,10 @@ setup(
     author_email = "komoto@jeol.co.jp",
     description = "A wrapper of matplotlib and wxPython (phoenix)",
     
-    long_description = readme,
+    url = "https://github.com/komoto48g/mwxlib",
+    
     ## long_description_content_type = "text/markdown",
+    long_description = open('README.rst').read(),
     
     ## Description of the package in the distribution
     package_dir = {
@@ -29,21 +25,22 @@ setup(
         'mwx',
     ],
     
-    install_requires = [
-        'numpy',
-        'scipy',
-        'pillow',
-        'matplotlib',
-        'wxpython',
-        'opencv-python',
-    ],
+    ## install_requires = [
+    ##     'wxpython',
+    ##     'numpy',
+    ##     'scipy',
+    ##     'pillow',
+    ##     'matplotlib',
+    ##     'opencv-python',
+    ## ],
+    install_requires = open("requirements.txt").read().splitlines(),
     
     ## This is necessary for egg distribution to include *.txt files
     package_data={
         "mwx": [
+            # no *.txt files to be included
         ],
     },
-    
     include_package_data = True,
     
     license = 'MIT',
@@ -57,9 +54,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering :: Image Processing',
-        'Topic :: System :: Shells',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     ## entry_points = {
     ##     'console_scripts': ['mwxlib = mwx.framework:deb']
