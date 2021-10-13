eb6dd5d00841290742407d7b8926dd55b44d05c7
komoto <komoto@jeol.co.jp>
2021-10-13 17:59:55 +0900

    Add plugman.py

---
+++ Lib/mwx/plugman.py
@@ -0,0 +1,257 @@
+#! python3
+# -*- coding: utf-8 -*-
+"""Plug manager
+
+Author: Kazuya O'moto <komoto@jeol.co.jp>
+"""
+import copy
+import wx
+from . import framework as mwx
+from .controls import Button
+from .graphman import Layer, Icon
+from .framework import CtrlInterface, TreeList
+
+
+class ItemData(Layer):
+    """Item data for TreeCtrl
+    
+Attributes:
+     status : status of the plugin (default -1)
+     symbol : map of status:icons idx
+   callback : a function which is called from menu
+       root : owner tree plugin object (to be overridden)
+    execute : funtion callback in subprocess (to be overridden)
+    """
+    menu = None
+    category = None
+    caption = property(lambda self: self.__module__)
+    
+    @property
+    def section(self):
+        return "{}/{}".format(self.category, self.caption)
+    
+    @property
+    def tree(self):
+        return self.root.tree
+    
+    def Init(self):
+        self.layout(None, (
+            Button(self, "Execute",
+                handler=lambda v: self.callback(), icon='->'),
+            ),
+        )
+        self.symbol = {-3:6, -2:2, -1:1, False:3, True:4, None:5,}
+        self.status = -1
+        
+        self.callback = self.root.thread(self.call_subprocess)
+        self.tree.append(self.section, self)
+    
+    def init_session(self, session):
+        self.item.status = session.get('status')
+    
+    def save_session(self, session):
+        session['status'] = self.item.status
+    
+    def Destroy(self):
+        try:
+            self.tree.remove(self.section)
+        finally:
+            ## wrapped C/C++ object has been deleted.
+            return Layer.Destroy(self)
+    
+    def __deepcopy__(self, memo):
+        """Called from deepcopy to export status"""
+        return self.status
+    
+    def update_status(self, status):
+        self.status = status
+        self.tree.SetItemImage(self.ItemId, self.symbol.get(status))
+    
+    def children(self):
+        """Generate items in the branch associated with this data:item"""
+        item, cookie = self.tree.GetFirstChild(self.ItemId)
+        while item.IsOk():
+            yield self.tree.GetItemData(item)
+            item, cookie = self.tree.GetNextChild(self.ItemId, cookie)
+    
+    def call_subprocess(self):
+        """Callback execution process
+        
+        ST_EXCEPTION = -3
+        ST_PROCESS   = -2
+        ST_NORMAL    = -1
+        * ST_FAILURE = False
+        * ST_SUCCESS = True
+        * ST_ERROR   = None
+        """
+        try:
+            print(self.root.message("[{}] is processing... "
+                                    "(Press C-g to quit)".format(self.section)))
+            self.update_status(-2)
+            ret = self.execute()
+            self.update_status(ret)
+            print(self.root.message("[{}] retval: {}".format(self.section, ret)))
+            return ret
+        except Exception as e:
+            print(self.root.message("- [{}] stopped: {}".format(self.section, e)))
+            self.update_status(-3)
+            raise
+
+
+class TreeCtrl(wx.TreeCtrl, CtrlInterface, TreeList):
+    icons = (
+        'folder',   # 0: 
+        'file',     # 1: (default)
+        '->',       # 2: (busy) in process
+        'w',        # 3: (false) 0=failure
+        'v',        # 4: (true) 1=success
+        '!!',       # 5: (nan) error
+        '!!!',      # 6: (nil) exception
+    )
+    def __init__(self, parent, **kwargs):
+        wx.TreeCtrl.__init__(self, parent, **kwargs)
+        CtrlInterface.__init__(self)
+        TreeList.__init__(self)
+        
+        self.li = wx.ImageList(16,16)
+        for icon in self.icons:
+            self.li.Add(Icon(icon, (16,16)))
+        self.SetImageList(self.li)
+        
+        self._root = None
+        
+        self.handler.update({
+            0 : {
+             '*Rbutton pressed' : (0, self.OnRightDown),
+               'Lbutton dclick' : (0, self.OnLeftDclick),
+                       'motion' : (0, self.OnMotion),
+            },
+        })
+        self.Bind(wx.EVT_MOTION, lambda v: self.handler('motion', v))
+    
+    def reset(self):
+        self.DeleteAllItems()
+        self._root = self.AddRoot("Root")
+        for branch in self:
+            self.set_item(self._root, *branch)
+    
+    def append(self, key, value):
+        self[key] = value
+        if not self._root:
+            return self.reset()
+        rootkey = key.partition('/')[0]
+        self.set_item(self._root, rootkey, self[rootkey])
+        self.Refresh()
+    
+    def remove(self, key):
+        del self[key]
+        self.reset()
+    
+    def get_item(self, parent, key):
+        if '/' in key:
+            a, b = key.split('/', 1)
+            child = self.get_item(parent, a)
+            return self.get_item(child, b)
+        
+        item, cookie = self.GetFirstChild(parent)
+        while item.IsOk():
+            if key == self.GetItemText(item):
+                return item
+            item, cookie = self.GetNextChild(parent, cookie)
+    
+    def set_item(self, parent, key, *values):
+        if '/' in key:
+            a, b = key.split('/', 1)
+            child = self.get_item(parent, a)
+            return self.set_item(child, b, *values)
+        
+        item = self.get_item(parent, key) or self.AppendItem(parent, key)
+        data = values[0]
+        if isinstance(data, ItemData):
+            self.SetItemData(item, data)
+            data.ItemId = item # set reference to the own item
+            
+            if len(values) == 1: # => (key, data)
+                data.update_status(data.status) # resotre session
+                return
+            
+            data.symbol[-1] = 0 # set default icon as 'folder'
+        
+        for branch in values[-1]: # => (key, data, branches)
+            self.set_item(item, *branch)
+        self.SetItemImage(item, image=0)
+    
+    def get_flags(self):
+        """Get all flags in the branches"""
+        return copy.deepcopy(list(self)) # => ItemData.__deepcopy__
+    
+    def set_flags(self, temp, target=None):
+        """Set temp flags to the target (or self) as most as possible
+        temp : TreeList template of flags to copy to the target
+        """
+        for org, branch in zip(temp, target or self):
+            tag, flags = org[0], org[-1]
+            key, data = branch[0], branch[-1]
+            if key != tag:
+                raise KeyError("Failed to restore status: "
+                               "got inconsistent keys {!r}, {!r})".format(tag, key))
+            if isinstance(data, ItemData):
+                data.update_status(flags)
+            else:
+                self.set_flags(flags, data)
+    
+    ## --------------------------------
+    ## TreeCtrl:Interface event handler
+    ## --------------------------------
+    
+    def OnMotion(self, evt):
+        item, flag = self.HitTest(evt.GetPosition())
+        if item.IsOk():
+            try:
+                data = self.GetItemData(item)
+                self.SetToolTip(data.__doc__)
+            except AttributeError:
+                self.SetToolTip("")
+        evt.Skip()
+    
+    def OnLeftDclick(self, evt):
+        item, flags = self.HitTest(evt.GetPosition())
+        if item.IsOk(): # and flags & (0x10|0x20|0x40|0x80):
+            try:
+                data = self.GetItemData(item)
+                data.Show()
+            except AttributeError:
+                wx.MessageBox("The selected item has no control panel")
+                pass
+        evt.Skip()
+    
+    def OnRightDown(self, evt):
+        item, flags = self.HitTest(evt.GetPosition())
+        if item.IsOk(): # and flags & (0x10|0x20|0x40|0x80):
+            self.SelectItem(item)
+            try:
+                data = self.GetItemData(item)
+                mwx.Menu.Popup(self.Parent, (
+                    (1, "clear", Icon(''),
+                        lambda v: data.update_status(-1)),
+                        
+                    (2, "execute", Icon('->'),
+                        lambda v: data.callback(),
+                        lambda v: v.Enable(data.callback is not None
+                                       and data.status is not True)),
+                    (),
+                    (3, "pass", Icon('v'),
+                        lambda v: data.update_status(True)),
+                        
+                    (4, "fail", Icon('w'),
+                        lambda v: data.update_status(False)),
+                        
+                    (5, "nil", Icon('x'),
+                        lambda v: data.update_status(None)),
+                    (),
+                    (6, "Maintenance for {!r}".format(data.__module__), Icon('proc'),
+                        lambda v: data.Show()),
+                ))
+            except AttributeError:
+                pass
+        evt.Skip()
