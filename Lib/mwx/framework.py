fabd05abebcf96bf5d55889e55293a95ca136d7b
komoto <komoto@jeol.co.jp>
2022-02-07 10:04:38 +0900

    Update version

--- Lib/mwx/framework.py
+++ Lib/mwx/framework.py
@@ -4,7 +4,7 @@
 
 Author: Kazuya O'moto <komoto@jeol.co.jp>
 """
-__version__ = "0.52.0rc2"
+__version__ = "0.52.0"
 __author__ = "Kazuya O'moto <komoto@jeol.co.jp>"
 
 from collections import OrderedDict
