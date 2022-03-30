089492b5b6f492d8fe149c5fd6c1eb50f505de00
komoto <komoto@jeol.co.jp>
2022-03-30 09:53:50 +0900

    Don't export Nautilus

--- Lib/mwx/__init__.py
+++ Lib/mwx/__init__.py
@@ -8,13 +8,13 @@ from .framework import apropos, typename
 from .framework import FSM
 from .framework import pack, hotkey
 from .framework import Menu, MenuBar, StatusBar
-from .framework import Frame, MiniFrame, ShellFrame, Nautilus
+from .framework import Frame, MiniFrame, ShellFrame
 from .framework import funcall # curry spices
 from .framework import deb
 
-## widgets
-## from .controls import Param, LParam, Knob, ControlPanel
-## from .controls import Icon, Button, ToggleButton, TextCtrl, Choice
+## from . import controls as wdigets
+## from .controls import Param, LParam, Knob, ControlPanel, Icon
+## from .controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator
 
 ## matplot
 ## from .matplot2 import MatplotPanel
@@ -23,9 +23,13 @@ from .framework import deb
 ## from .matplot2lg import Histogram
 ## from .matplot2lg import LineProfile
 
+## from . import graphman
+## from .graphman import Layer, Thread, Graph
+## from .graphman import Frame as Graphman
+
 ## Gnuplot
-## from .mgplt import Gplot
-## from .mgplt import GplotFrame
+## from .mgplt import Gnuplot
+## from .mgplt import GnuplotFrame
 
 ## mwx.reload
 from importlib import reload
