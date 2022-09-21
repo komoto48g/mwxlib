e7d5264e9571a88c7a231e5c8905b988b90a63ea
komoto <komoto@jeol.co.jp>
2022-09-21 13:30:01 +0900

    Modify Editor keymap; Add [Escape] AnnotationClear

--- Lib/mwx/framework.py
+++ Lib/mwx/framework.py
@@ -4,7 +4,7 @@
 
 Author: Kazuya O'moto <komoto@jeol.co.jp>
 """
-__version__ = "0.72.0"
+__version__ = "0.72.1"
 __author__ = "Kazuya O'moto <komoto@jeol.co.jp>"
 
 from functools import wraps, partial
