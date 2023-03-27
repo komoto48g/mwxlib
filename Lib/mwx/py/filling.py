bc5f62c4e8a5fc8e5e959d79a546c7d1bd880320
Kazuya O'moto <komoto@jeol.co.jp>
2023-03-27 13:42:52 +0900

    Fix filling item data comaprison

--- Lib/mwx/py/filling.py
+++ Lib/mwx/py/filling.py
@@ -175,7 +175,7 @@ class FillingTree(wx.TreeCtrl):
         for key in (B & A):
             i = items[key]
             child = children[key]
-            if self.GetItemData(i) != child:
+            if self.GetItemData(i) is not child:
                 self.SetItemData(i, child)
         for key in sorted(B - A):
             itemtext = six.text_type(key)
@@ -228,7 +228,7 @@ class FillingTree(wx.TreeCtrl):
                 try:
                     # data = self.objGetChildren(obj)[key] # overheads...
                     data = getattr(obj, key) # easier way to access here.
-                    if self.GetItemData(item) != data:
+                    if self.GetItemData(item) is not data:
                         self.SetItemData(item, data)
                         return item
                 except AttributeError:
