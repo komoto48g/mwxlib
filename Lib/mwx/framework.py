b5e87a9be54a98c85d6856131f7204ca479ffbe4
Kazuya O'moto <komoto@jeol.co.jp>
2023-05-17 19:09:33 +0900

    :mpl: Fix pane update_pane repositioning

--- Lib/mwx/framework.py
+++ Lib/mwx/framework.py
@@ -4,7 +4,7 @@
 
 Author: Kazuya O'moto <komoto@jeol.co.jp>
 """
-__version__ = "0.83.2"
+__version__ = "0.83.3"
 __author__ = "Kazuya O'moto <komoto@jeol.co.jp>"
 
 from functools import wraps, partial
