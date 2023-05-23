0330791e6c33379e4ed2138d5761a22e230165a6
Kazuya O'moto <komoto@jeol.co.jp>
2023-05-24 01:20:52 +0900

    :mpl: Remove postcall in Layer.Show

--- Lib/mwx/graphman.py
+++ Lib/mwx/graphman.py
@@ -381,7 +381,7 @@ class LayerInterface(CtrlInterface):
     
     def Show(self, show=True):
         """Show associated pane (override) window."""
-        wx.CallAfter(self.parent.show_pane, self, show)
+        self.parent.show_pane(self, show)
     
     Drawn = property(
         lambda self: self.IsDrawn(),
