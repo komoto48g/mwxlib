dbcdf209535b42f6d8fc2a70f649509339cb5266
Kazuya O'moto <komoto@jeol.co.jp>
2023-10-12 11:15:39 +0900

    Remove __getattr__

--- Lib/mwx/utilus.py
+++ Lib/mwx/utilus.py
@@ -881,18 +881,11 @@ class TreeList(object):
                 ...]],
         ]
     """
-    ## A dummy list to avoid RecursionError occurs when
-    ## __getattr__ may be called before __init__.
-    __items = None
-    
     def __init__(self, ls=None):
         self.__items = ls or []
     
     def __call__(self, k):
         return TreeList(self[k])
-    
-    def __getattr__(self, attr):
-        return getattr(self.__items, attr)
     
     def __contains__(self, k):
         return self.getf(self.__items, k)
