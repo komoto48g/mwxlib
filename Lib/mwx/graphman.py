b88f74afd34013633d033d5f51693b7cd918d642
Kazuya O'moto <komoto@jeol.co.jp>
2024-01-23 23:46:56 +0900

    Don't load file in inspect_plug

--- Lib/mwx/graphman.py
+++ Lib/mwx/graphman.py
@@ -1344,7 +1344,6 @@ class Frame(mwx.Frame):
             plug = _plug
         init(shell)
         self.shellframe.Show()
-        self.shellframe.load(plug)
     
     def OnLoadPlugins(self, evt):
         with wx.FileDialog(self, "Load a plugin file",
