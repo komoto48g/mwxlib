45fcd22c213dfbf4f818767ad9d4d69596841fb8
Kazuya O'moto <komoto@jeol.co.jp>
2024-04-24 17:20:34 +0900

    Move deb to framework

--- Lib/mwx/__init__.py
+++ Lib/mwx/__init__.py
@@ -4,7 +4,7 @@
 from .framework import __version__, __author__
 from .framework import FSM
 from .framework import Menu, MenuBar, StatusBar
-from .framework import App, Frame, MiniFrame, ShellFrame
+from .framework import deb, App, Frame, MiniFrame, ShellFrame
 
 ## Controls
 ## from . import controls
@@ -25,46 +25,3 @@ from .framework import App, Frame, MiniFrame, ShellFrame
 ## Gnuplot
 ## from .mgplt import Gnuplot
 ## from .mgplt import GnuplotFrame
-
-
-def deb(target=None, loop=True, locals=None, **kwargs):
-    """Dive into the process.
-    
-    Args:
-        target  : Object or module (default None).
-                  If None, the target is set to `__main__`.
-        loop    : If True, the app and the mainloop will be created.
-                  Otherwise, neither the app nor the mainloop will be created.
-        locals  : Additional context of the shell
-        
-        **kwargs: Nautilus arguments
-        
-            - introText         : introductory of the shell
-            - startupScript     : startup script file (default None)
-            - execStartupScript : True => Execute the startup script.
-            - ensureClose       : True => EVT_CLOSE will close the window.
-                                  False => EVT_CLOSE will hide the window.
-    
-    Note:
-        This will execute the startup script $(PYTHONSTARTUP).
-    """
-    import wx
-    
-    quote_unqoute = """
-        Anything one man can imagine, other man can make real.
-        --- Jules Verne (1828--1905)
-        """
-    kwargs.setdefault("introText",
-                      "mwx {}".format(__version__) + quote_unqoute)
-    kwargs.setdefault("execStartupScript", True)
-    kwargs.setdefault("ensureClose", True)
-    
-    app = wx.GetApp() or wx.App()
-    frame = ShellFrame(None, target, **kwargs)
-    frame.Show()
-    frame.rootshell.SetFocus()
-    if locals:
-        frame.rootshell.locals.update(locals)
-    if loop and not app.GetMainLoop():
-        app.MainLoop()
-    return frame
--- Lib/mwx/framework.py
+++ Lib/mwx/framework.py
@@ -1,7 +1,7 @@
 #! python3
 """mwxlib framework.
 """
-__version__ = "0.94.1"
+__version__ = "0.94.2"
 __author__ = "Kazuya O'moto <komoto@jeol.co.jp>"
 
 from functools import wraps, partial
@@ -24,6 +24,47 @@ from wx.py import dispatcher
 from .utilus import funcall as _F
 from .utilus import FSM, TreeList, apropos, typename, where, mro, pp
 from .utilus import get_rootpath
+
+
+def deb(target=None, loop=True, locals=None, **kwargs):
+    """Dive into the process.
+    
+    Args:
+        target  : Object or module (default None).
+                  If None, the target is set to `__main__`.
+        loop    : If True, the app and the mainloop will be created.
+                  Otherwise, neither the app nor the mainloop will be created.
+        locals  : Additional context of the shell
+        
+        **kwargs: Nautilus arguments
+        
+            - introText         : introductory of the shell
+            - startupScript     : startup script file (default None)
+            - execStartupScript : True => Execute the startup script.
+            - ensureClose       : True => EVT_CLOSE will close the window.
+                                  False => EVT_CLOSE will hide the window.
+    
+    Note:
+        This will execute the startup script $(PYTHONSTARTUP).
+    """
+    import wx
+    
+    quote_unqoute = """
+        Anything one man can imagine, other man can make real.
+        --- Jules Verne (1828--1905)
+        """
+    kwargs.setdefault("introText",
+                      "mwx {}".format(__version__) + quote_unqoute)
+    kwargs.setdefault("execStartupScript", True)
+    kwargs.setdefault("ensureClose", True)
+    
+    with App(loop):
+        frame = ShellFrame(None, target, **kwargs)
+        frame.Show()
+        frame.rootshell.SetFocus()
+        if locals:
+            frame.rootshell.locals.update(locals)
+        return frame
 
 
 @contextmanager
