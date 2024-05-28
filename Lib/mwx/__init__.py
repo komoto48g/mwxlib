59a57f249225fb0a6cfacd7fdf5af761d883ff93
Kazuya O'moto <komoto@jeol.co.jp>
2024-05-29 01:11:52 +0900

    Add controls to mwx.__init__.py; Udpate version

--- Lib/mwx/__init__.py
+++ Lib/mwx/__init__.py
@@ -8,12 +8,12 @@ from .framework import Frame, MiniFrame, ShellFrame, deb
 
 ## Controls
 ## from . import controls
-## from .controls import Param, LParam, Knob, ControlPanel, Clipboard, Icon
-## from .controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator
+from .controls import Param, LParam, Knob, ControlPanel, Clipboard, Icon
+from .controls import Button, ToggleButton, TextCtrl, Choice, Gauge, Indicator
 
 ## Plugman
 ## from . import graphman
-## from .graphman import Frame, Layer, Thread, Graph
+## from .graphman import Frame as GraphFrame, Layer, Thread, Graph
 
 ## Matplot
 ## from .matplot2 import MatplotPanel
@@ -23,5 +23,4 @@ from .framework import Frame, MiniFrame, ShellFrame, deb
 ## from .matplot2lg import LineProfile
 
 ## Gnuplot
-## from .mgplt import Gnuplot
-## from .mgplt import GnuplotFrame
+## from .mgplt import Gnuplot, GnuplotFrame
