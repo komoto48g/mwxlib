2a0d997a39c51b114f60e58519d0575aaf460f71
Kazuya O'moto <komoto@jeol.co.jp>
2024-11-06 11:44:41 +0900

    Add testsuite module

---
+++ Lib/mwx/testsuite.py
@@ -0,0 +1,58 @@
+#! python3
+"""Test suite for App, Frame, and ControlPanel.
+
+Get the wx.App or wx.Frame instance and start the main-loop if needed.
+
+Usage:
+    with testApp() as app:
+        frm = wx.Frame(None)
+        frm.Show()
+
+Is equivlent to:
+    app = wx.App()
+    frm = wx.Frame(None)
+    frm.Show()
+    app.MainLoop()
+"""
+from contextlib import contextmanager
+import wx
+
+__all__ = ["testApp", "testFrame", "Plugman", "ControlPanel"]
+
+
+@contextmanager
+def testApp():
+    app = wx.GetApp() or wx.App()
+    yield app
+    if not app.GetMainLoop():
+        app.MainLoop()
+## wx.App.run = staticmethod(testApp)
+
+
+@contextmanager
+def testFrame(**kwargs):
+    with testApp():
+        frm = wx.Frame(None, **kwargs)
+        yield frm
+        frm.Show()
+## wx.Frame.run = staticmethod(testFrame)
+
+
+@contextmanager
+def Plugman(**kwargs):
+    import mwx.graphman
+    with testApp():
+        frm = mwx.graphman.Frame(None, **kwargs)
+        yield frm
+        frm.Show()
+
+
+@contextmanager
+def ControlPanel(**kwargs):
+    import mwx
+    with testApp():
+        frm = mwx.Frame(None)
+        panel = mwx.ControlPanel(frm, **kwargs)
+        yield panel
+        panel.Sizer.Fit(frm)
+        frm.Show()
